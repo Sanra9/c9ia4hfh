a0d50a046ec6ffdb84d94e9ea4c3dc85d24fa6c4
sanra9 <sanrra999@gmail.com>
2018-05-30 18:19:55 -0500

    Subiendo proyecto a git

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150510183516) do
+ActiveRecord::Schema.define(version: 20180530231833) do
 
   create_table "rooms", force: :cascade do |t|
     t.string   "title"
